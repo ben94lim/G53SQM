e41f1c943f192c82a818d4d704573cb2c80a5f90
Benjamin Lim <ben94lim@gmail.com>
2015-12-04 15:22:04 +0800

    Added test for "getHashHeader" and "getHashMap" methods

--- src/test/java/g53sqm/jibble/RequestThreadTest.java
+++ src/test/java/g53sqm/jibble/RequestThreadTest.java
@@ -5,7 +5,18 @@ package g53sqm.jibble;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.util.HashMap;
+
 import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author User
@@ -31,5 +42,35 @@ public class RequestThreadTest {
 
 		assertEquals("usable", RequestThread.readFirstLine(usableLine));
 	}
+	
+	@Test
+	public void getHashHeaderTest() {
+		String[] output = new String[2];
+		
+		String emptyLine = "";		
+		output = RequestThread.getHashHeader(emptyLine);
+		assertEquals("", output[0]);
+		
+		String usableLine = "Tag:123456";
+		
+		output = RequestThread.getHashHeader(usableLine);
+		assertEquals("Tag", output[0]);
+		assertEquals("123456", output[1]);
+	}
 
+	@Test
+	public void getHashMapTest() throws UnsupportedEncodingException {
+		String response = "Tag1:1234 \n Tag2:5678";
+		String encoding = "UTF-8";
+
+
+		InputStream inputStream = new ByteArrayInputStream(response.getBytes(encoding));
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+		HashMap <String, String> headers = RequestThread.getHashMap(in);
+		assertEquals("1234", headers.get("Tag1")); // message sent and got a response
+		assertEquals("5678", headers.get("Tag2"));
+
+	}
 }
