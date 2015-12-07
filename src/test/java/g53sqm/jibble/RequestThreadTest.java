01725d27bb1a9b766a2dd5abb2fa8f0233883eab
Benjamin Lim <ben94lim@gmail.com>
2015-12-07 20:48:31 +0800

    Added test for "checkFile" method

--- src/test/java/g53sqm/jibble/RequestThreadTest.java
+++ src/test/java/g53sqm/jibble/RequestThreadTest.java
@@ -7,14 +7,19 @@ import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.Socket;
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Logger;
 import junitparams.*;
 
 import static org.mockito.Mockito.*;
@@ -25,6 +30,15 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(JUnitParamsRunner.class)
 public class RequestThreadTest {
+	Socket socket;
+	RequestThread testClass;
+	
+	@Before
+	public void setUp() throws Exception {
+		Socket socket = mock(Socket.class);
+		File rootDir = new File("/");
+		testClass = new RequestThread(socket, rootDir);
+	}
 
 	private Object[] requests()
 	{
@@ -40,7 +54,7 @@ public class RequestThreadTest {
 	@Test
 	@Parameters(method = "requests")
 	public void readFirstLineTest(String requests, String expected) {
-		assertEquals(expected, RequestThread.readFirstLine(requests));
+		assertEquals(expected, testClass.readFirstLine(requests));
 	}
 	
 	@Test
@@ -48,12 +62,12 @@ public class RequestThreadTest {
 		String[] output = new String[2];
 		
 		String emptyLine = "";		
-		output = RequestThread.getHashHeader(emptyLine);
+		output = testClass.getHashHeader(emptyLine);
 		assertEquals("", output[0]);
 		
 		String usableLine = "Tag:123456";
 		
-		output = RequestThread.getHashHeader(usableLine);
+		output = testClass.getHashHeader(usableLine);
 		assertEquals("Tag", output[0]);
 		assertEquals("123456", output[1]);
 	}
@@ -68,9 +82,23 @@ public class RequestThreadTest {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
 
-		HashMap <String, String> headers = RequestThread.getHashMap(in);
+		HashMap <String, String> headers = testClass.getHashMap(in);
 		assertEquals("1234", headers.get("Tag1")); // message sent and got a response
 		assertEquals("5678", headers.get("Tag2"));
-
+	}
+	
+	private Object[] files()
+	{
+		return new Object[]
+		{				
+				new Object[] {new File ("src/main/resources/webfiles"), 1},
+				new Object[] {new File ("/nonExistent.txt"), 2},
+				new Object[] {new File ("/"), 3}
+		};
+	}
+	@Test
+	@Parameters(method = "files")
+	public void checkFileTest(File files, int expected) {
+		assertEquals(expected, testClass.checkFile(files));
 	}
 }
