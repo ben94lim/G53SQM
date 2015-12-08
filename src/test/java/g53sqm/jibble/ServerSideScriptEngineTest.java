aa59d2ce9ee61191c591dc703dd96ff1e49c3de1
Benjamin Lim <ben94lim@gmail.com>
2015-12-08 15:24:55 +0800

    Added test for "extractVariables" method

---
+++ src/test/java/g53sqm/jibble/ServerSideScriptEngineTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package g53sqm.jibble;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Benjamin Lim
+ *
+ */
+public class ServerSideScriptEngineTest {
+
+	@Test
+	public void extractVariableTest() {
+		fail("Not yet implemented");
+	}
+
+}
